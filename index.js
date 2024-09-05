4e1d074c865331618c0eea7ab785c2895d03190b
salomia <salomia.razakarivony@gmail.com>
2024-09-05 17:31:39 +0300

    fixed: cors

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ const __dirname = path.dirname(__filename);
 
 app.use(bodyParser())
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// });
 
 
 app.use((req, res, next) => {
