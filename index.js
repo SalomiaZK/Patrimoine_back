33001a4e42d8761c562cfd6f090743c9eeb5fa03
salomia <salomia.razakarivony@gmail.com>
2024-09-05 12:43:08 +0300

    fixed: cors

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://patrimoine-eco-ui.onrender.com/');
+    res.header('Access-Control-Allow-Origin', 'https://patrimoine-eco-ui.onrender.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
